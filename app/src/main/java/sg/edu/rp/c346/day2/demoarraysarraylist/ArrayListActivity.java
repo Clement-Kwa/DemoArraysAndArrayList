e49246dd7259c490aae038d8db9c5334d561c126
20002694 <20002694@myrp.edu.sg>
2021-06-03 10:46:44 +0800

    ws1

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
@@ -17,7 +19,20 @@ public class ArrayListActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView2);
 
         //continue view the code in the worksheet
+        ArrayList<String> fruits = new ArrayList<String>();
 
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+
+
+        tv.setText("Fruits\n====\n");
+
+        for(int i=0; i<fruits.size(); i++){
+            tv.setText(tv.getText()+fruits.get(i)+"\n");
+        }
+
+        fruits.set(fruits.size(),"Dragon fruit");
 
     }
 }
